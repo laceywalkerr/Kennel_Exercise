c270932baac6a610da32d0a20bcf43cfb184838d
Lacey Walker <65372269+laceywalkerr@users.noreply.github.com>
2020-07-13 00:51:29 -0500

    fixed bug, replaced classname with className in navbar code

--- src/components/animal/AnimalList.js
+++ src/components/animal/AnimalList.js
@@ -1,23 +1,30 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
+//import the components we will need
+import AnimalCard from './AnimalCard';
 import AnimalManager from '../../modules/AnimalManager';
 
 const AnimalList = () => {
+  // The initial state is an empty array
+  const [animals, setAnimals] = useState([]);
+
   const getAnimals = () => {
+    // After the data comes back from the API, we
+    //  use the setAnimals function to update state
     return AnimalManager.getAll().then(animalsFromAPI => {
-      // We'll do something more interesting with this data soon.
-      console.log(animalsFromAPI);
+      setAnimals(animalsFromAPI)
     });
   };
 
+  // got the animals from the API on the component's first render
   useEffect(() => {
     getAnimals();
   }, []);
 
+  // Finally we use map() to "loop over" the animals array to show a list of animal cards
   return (
     <div className="container-cards">
-      We'll put some animals here eventually...
+      {animals.map(animal => <AnimalCard />)}
     </div>
   );
 };
-
 export default AnimalList
