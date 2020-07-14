019741f98913241f3f388f275d7f12aef814d786
Lacey Walker <65372269+laceywalkerr@users.noreply.github.com>
2020-07-14 17:04:09 -0500

    animal spotlight added

--- src/components/animal/AnimalSpotlight.js
+++ src/components/animal/AnimalSpotlight.js
@@ -1,28 +1,28 @@
-// import React, { useState, useEffect } from "react";
-// import AnimalManager from "../../modules/AnimalManager";
-// import "./AnimalSpotlight.css";
+import React, { useState, useEffect } from "react";
+import AnimalManager from "../../modules/AnimalManager";
+import "./AnimalSpotlight.css";
 
-// const AnimalSpotlight = props => {
-//   const [animal, setAnimal] = useState({ name: "", breed: "" });
+const AnimalSpotlight = props => {
+  const [animal, setAnimal] = useState({ name: "", breed: "" });
 
-//   useEffect(() => {
-//     AnimalManager.get(props.animalId).then(animal => {
-//       setAnimal({
-//         name: animal.name,
-//         breed: animal.breed
-//       });
-//     });
-//   }, [props.animalId]);
+  useEffect(() => {
+    AnimalManager.get(props.animalId).then(animal => {
+      setAnimal({
+        name: animal.name,
+        breed: animal.breed
+      });
+    });
+  }, [props.animalId]);
 
-//   return (
-//     <div className="animal-spotlight">
-//       <img src={require('./dog.svg')} alt="My Dog" />
-//       <div>
-//         <h3>{animal.name}</h3>
-//         <p>{animal.breed}</p>
-//       </div>
-//     </div>
-//   );
-// };
+  return (
+    <div className="animal-spotlight">
+      <img src={require('./dog.svg')} alt="My Dog" />
+      <div>
+        <h3>{animal.name}</h3>
+        <p>{animal.breed}</p>
+      </div>
+    </div>
+  );
+};
 
-// export default AnimalSpotlight;
+export default AnimalSpotlight;
